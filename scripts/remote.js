c792eee003229dc1157612e8487eed3eeda45ad7
Eduardo Gomez <egomez3412@gmail.com>
2021-03-31 20:58:38 -0700

    index override

---
+++ scripts/remote.js
@@ -0,0 +1,8 @@
+db.collection('bets').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        datas = doc.data();
+        console.log(doc.data());
+        var span = '<span>' + datas.points + '</span>';
+        document.getElementById("firebase-test").innerHTML += 'Content: ' + span + '<br>'; 
+    });
+});
